5d0f8f0b8262ace51d4fd1fb307aefcae8ae9b4e
kluesky <140289431+kluesky@users.noreply.github.com>
2024-03-05 19:22:23 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,23 +15,23 @@ global.sessionid ='ur session id'
 
 //owmner v card
 global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.socialm = "Instagram: @me_kyluesky" //ur github or insta name
+global.location = "Japan, Nagoya, Yagoto" //ur location
 
 //new
-global.botname = 'Cheems Bot MD V11' //ur bot name
-global.ownernumber = '916909137213' //ur owner number
-global.ownername = '🦄드림 가이 Xeon' //ur owner name
-global.websitex = "https://youtu.be/mu5m6aB6P5k"
-global.wagc = "https://whatsapp.com/channel/0029VaG9VfPKWEKk1rxTQD20"
+global.botname = 'Seiren Beta' //ur bot name
+global.ownernumber = '6281994421192' //ur owner number
+global.ownername = 'Seira' //ur owner name
+global.websitex = "https://www.youtube.com/@Kyluesky"
+global.wagc = "https://whatsapp.com/channel/0029VaP7Yk2KbYMSCUOFS440"
 global.themeemoji = '🪀'
-global.wm = "Xeon Bot Inc."
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD11' //script link
+global.wm = "Seiren Bot Inc."
+global.botscript = 'https://github.com/kluesky/Kyluesky1' //script link
 global.packname = "Sticker By"
-global.author = "🦄드림 가이 Xeon\n\n+916909137213"
-global.creator = "916909137213@s.whatsapp.net"
+global.author = "Seiren\n\n+6281994421192"
+global.creator = "6281994421192@s.whatsapp.net"
 global.xprefix = '.'
-global.premium = ["916909137213"] // Premium User
+global.premium = ["6281994421192"] // Premium User
 global.hituet = 0
 
 //bot sett
@@ -39,10 +39,10 @@ global.typemenu = 'v8' // menu type 'v1' => 'v8'
 global.typereply = 'v2' // reply type 'v1' => 'v3'
 global.autoblocknumber = '92' //set autoblock country code
 global.antiforeignnumber = '91' //set anti foreign number country code
-global.welcome = false //welcome/left in groups
-global.anticall = false //bot blocks user when called
+global.welcome = true //welcome/left in groups
+global.anticall = true //bot blocks user when called
 global.autoswview = false //auto status/story view
-global.adminevent = false //show promote/demote message
+global.adminevent = true //show promote/demote message
 global.groupevent = false //show update messages in group chat
 //msg
 global.mess = {
